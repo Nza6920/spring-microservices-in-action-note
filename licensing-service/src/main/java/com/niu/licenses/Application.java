a977805c403f65b7c49bb164e13df4fafd1be58f
NiuZian <zian.niu@gdcccn.com>
2021-03-02 17:56:31 +0800

    许可服务集成配置服务

--- licensing-service/src/main/java/com/niu/licenses/Application.java
+++ licensing-service/src/main/java/com/niu/licenses/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 启动类
  *
- * @version 1.0 [2021/03/01 10:46]
  * @author [nza]
+ * @version 1.0 [2021/03/01 10:46]
  * @createTime [2021/03/01 10:46]
  */
 @SpringBootApplication
@@ -16,4 +16,22 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        // 参数：
+//        // jdbc协议:postgresql子协议://主机地址:数据库端口号/要连接的数据库名
+//        String url = "jdbc:postgresql://42.193.117.17:5432/test";
+//        // 数据库用户名
+//        String user = "postgres";
+//        // 数据库密码
+//        String password = "nza08157016";
+//
+//        // 1. 加载Driver类，Driver类对象将自动被注册到DriverManager类中
+//        Class.forName("org.postgresql.Driver");
+//
+//        // 2. 连接数据库，返回连接对象
+//        Connection conn = DriverManager.getConnection(url, user, password);
+//
+//        System.out.println("链接成功");
+//    }
 }
